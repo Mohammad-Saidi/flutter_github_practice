3b367b21e8dac825503e6854c71bab0a862c88be
Mohammad-Saidi <mohammadsaidi1995@gmail.com>
2025-01-11 23:11:20 +0600

    version 1.0.3

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,3 +1,4 @@
 // This file is edited at github from cloud
 // Again edited to check second time update button at android studio Ide
 // Again updating to check the below button at IDE
+// Saidi branch created and edited at controller file
